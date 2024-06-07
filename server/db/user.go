47f4bb39b0f7583557b637ceb4a7658aed14cdfc
Mitsuhiro-Usesugi <1792887657@qq.com>
2024-06-07 17:18:40 +0900

    user

--- server/db/user.go
+++ server/db/user.go
@@ -1,3 +1,16 @@
 package db
 
-type User struct{}
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	UserName      string    `gorm:"not null"`
+	PassWord      string    `gorm:"not null"`
+	Gender        string    `gorm:"not null"`
+	Motto         string    `gorm:"not null"`
+	LastLoginTime time.Time ``
+}
