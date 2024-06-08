d732b34fa94f586ae2c47fd96bd363fa7e40f5fc
Mitsuhiro-Usesugi <1792887657@qq.com>
2024-06-08 12:36:55 +0900

    class defined

--- server/db/user.go
+++ server/db/user.go
@@ -8,9 +8,31 @@ import (
 
 type User struct {
 	gorm.Model
-	UserName      string    `gorm:"not null"`
+	UserName      string    `gorm:"unique;not null"`
 	PassWord      string    `gorm:"not null"`
 	Gender        string    `gorm:"not null"`
 	Motto         string    `gorm:"not null"`
-	LastLoginTime time.Time ``
+	RegisterTime  time.Time `gorm:"not null"`
+	LastLoginTime time.Time `gorm:"not null"`
+	Avatar        string    `gorm:"default:null"`
+	NumofShares   uint      `gorm:"default:0"`
+	UserClass     uint      `gorm:"default:0"`
 }
+
+type UserGet struct {
+	UserName      string
+	Gender        string
+	Motto         string
+	RegisterTime  time.Time
+	LastLoginTime time.Time
+	Avatar        string
+	NumofShares   uint
+	UserClass     uint
+}
+
+type UserLogin struct {
+	UserName string
+	Password string
+}
+
+type
